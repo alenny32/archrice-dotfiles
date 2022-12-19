9f15f332f16ef792c08324cc8e83df39359fd001
Umar Ibrahim <umaribrahim435@gmail.com>
2022-12-20 02:59:47 +0700

    fix mv filename matching

--- stow-compat.sh
+++ stow-compat.sh
@@ -1,6 +1,8 @@
+#!/usr/bin/env bash
+
 # Script for making dotfiles compatible for stow
 
 for d in */ ; do
 	[ ! -d $d/.config/$d ] && mkdir -pv $d/.config/$d
-	mv -fv $d/* $d/.config/$d
+	mv -fv $d/.* $d/.config/$d
 done
