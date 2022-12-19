c0ef8077905d0cb72faa8db1e1fbef26d3df64fe
Umar Ibrahim <umaribrahim435@gmail.com>
2022-12-20 03:09:05 +0700

    fix zsh dotfiles dirs

--- stow-compat.sh
+++ stow-compat.sh
@@ -6,3 +6,8 @@ for d in */ ; do
 	[ ! -d $d/.config/$d ] && mkdir -pv $d/.config/$d
 	mv -fv $d/* $d/.config/$d
 done
+
+# move [dot]files in zsh
+for f in zsh/.* ; do
+	mv -fv $f zsh/.config/zsh/
+done
