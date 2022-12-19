73ba60f21cfb7941b989e856a5f0f57d4a3cf4b9
Umar Ibrahim <umaribrahim435@gmail.com>
2022-12-20 02:37:04 +0700

    optimise copy script

--- copy-config.sh
+++ copy-config.sh
@@ -1,19 +1,16 @@
 # Copy .config dir from archrice
 
-# Remove all dir first then copy it again from archrice repo
+# Remove all dirs and user* file first then copy it again from archrice repo
 
 alias \
 	rm="rm -rfv" \
 	cp="cp -rfv" \
 
-rm alacritty
-rm bspwm
-rm fontconfig
-rm gtk*
-rm guake
-rm JetBrains
-rm mpv
-rm nitrogen
-rm nvim
+rm user*
+
+for d in */ ; do
+	[ "$d" = ".git" ] && continue
+	rm $d
+done
 
 cp ~/Repos/archrice/.config/* .
