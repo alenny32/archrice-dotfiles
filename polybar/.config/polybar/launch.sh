40fb8c42ed1257e505c39a2cf9edc2b9601b10ff
Umar Ibrahim <umaribrahim435@gmail.com>
2022-12-21 11:50:39 +0700

    fix bar name

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -7,6 +7,6 @@ killall -q polybar
 while pgrep -u $UID -x polybar 2>/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar &
+polybar top 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Polybar launched...
