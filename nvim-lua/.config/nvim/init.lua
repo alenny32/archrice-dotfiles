ffdae73031c693a2335a1f3d5db0d083a71157af
Umar Ibrahim <umaribrahim435@gmail.com>
2023-01-07 12:16:21 +0700

    dont run vim plugins in vscode

--- nvim-lua/.config/nvim/init.lua
+++ nvim-lua/.config/nvim/init.lua
@@ -2,7 +2,8 @@ require("general")          -- General Settings
 require("mappings")         -- Keymaps
 require("commands")         -- Colourscheme and other highlights
 require("functions")        -- function definitions
-require("plugins")          -- Vim-Plug auto installer
 require("macros")           -- Macro definitions
-
-CycleThemes()
+if not vim.g.vscode then
+    require("plugins")          -- Vim-Plug auto installer
+    CycleThemes()
+end
