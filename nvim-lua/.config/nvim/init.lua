6d39e2118f8ff5097e39c8d53d4e257ea56bac0c
Umar Ibrahim <umaribrahim435@gmail.com>
2023-01-13 14:20:25 +0700

    remove "move line" keybinding if using vscode

--- nvim-lua/.config/nvim/init.lua
+++ nvim-lua/.config/nvim/init.lua
@@ -3,7 +3,12 @@ require("mappings")         -- Keymaps
 require("commands")         -- Colourscheme and other highlights
 require("functions")        -- function definitions
 require("macros")           -- Macro definitions
-if not vim.g.vscode then
+if vim.g.vscode then
+    vim.cmd("map <A-j> <Nop>")
+    vim.cmd("imap <A-j> <Nop>")
+    vim.cmd("map <A-k> <Nop>")
+    vim.cmd("imap <A-k> <Nop>")
+else
     require("plugins")          -- Vim-Plug auto installer
     CycleThemes()
 end
